2bbe30bbe21a0975569420fc03c56ea3c44578fd
Darin Allen <darinallen@gmail.com>
2017-03-11 15:27:07 -0500

    Implement navigation

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,14 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+
+  $scope.link = {
+    url: ''
+  };
+
+  $scope.addLink = function() {
+    $scope.link.url = $scope.url;
+    Links.addOne($scope.link);
+    $scope.url = '';
+  };
 });
